ee2981fe929bf9aba26644330f36c3d70673405e
AlinaProkofeva <103901357+AlinaProkofeva@users.noreply.github.com>
2022-10-02 19:48:55 +0300

    Add files via upload

---
+++ models_6.py
@@ -0,0 +1,67 @@
+import sqlalchemy as sq
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import declarative_base, relationship
+
+Base = declarative_base()
+
+class Publisher(Base):
+    __tablename__ = 'publisher'
+
+    id = sq.Column(sq.Integer, primary_key=True)
+    name = sq.Column(sq.String(80), nullable=False, unique=True)
+
+    def __str__(self):
+        return f'Publisher id_{self.id} "{self.name}"'
+
+class Book(Base):
+    __tablename__ = 'book'
+
+    id = sq.Column(sq.Integer, primary_key=True)
+    title = sq.Column(sq.Text, nullable=False)
+    id_publisher = sq.Column(sq.Integer, ForeignKey('publisher.id'), nullable=False)
+
+    publisher = relationship(Publisher, backref='book')
+
+    def __str__(self):
+        return f'Book "{self.title}"'
+
+class Shop(Base):
+    __tablename__ = 'shop'
+
+    id = sq.Column(sq.Integer, primary_key=True)
+    name = sq.Column(sq.String(80), nullable=False, unique=True)
+
+    def __str__(self):
+        return f'Shop "{self.name}"'
+
+class Stock(Base):
+    __tablename__ = 'stock'
+
+    id = sq.Column(sq.Integer, primary_key=True)
+    id_book = sq.Column(sq.Integer, ForeignKey('book.id'), nullable=False)
+    id_shop = sq.Column(sq.Integer, ForeignKey('shop.id'), nullable=False)
+    count = sq.Column(sq.Integer, nullable=False)
+
+    book = relationship(Book, backref='stock')
+    shop = relationship(Shop, backref='stock')
+
+class Sale(Base):
+    __tablename__ = 'sale'
+
+    id = sq.Column(sq.Integer, primary_key=True)
+    price = sq.Column(sq.Float, nullable=False)
+    date_sale= sq.Column(sq.Date, nullable=False)
+    id_stock = sq.Column(sq.Integer, ForeignKey('stock.id'), nullable=False)
+    count = sq.Column(sq.Integer, nullable=False)
+
+    stock = relationship(Stock, backref='sale')
+
+    def __str__(self):
+        return f'Sale {self.id} {self.date_sale}'
+
+def create_tables(engine):
+    Base.metadata.drop_all(engine)
+    Base.metadata.create_all(engine)
+
+
+
